01fbc50d48ceacc13fb4505a0ef32f1af4c346b9
Steve Yen <steve.yen@gmail.com>
2011-04-16 11:08:46 -0700

    optx fix

--- sql-bubble.rb
+++ sql-bubble.rb
@@ -5,16 +5,15 @@
 bubble = ARGV[0] || 'sql-bubble.txt'
 
 lines = File.open(bubble, "r").readlines
-lines = lines.map {|x| x.gsub(/\{\}/, '')}
 lines = lines.reject {|x| x.match(/^\s*\#/)} # Comments.
 lines = lines.reject {|x| x.match(/^\s*$/)}  # Whitespace.
+lines = lines.map {|x| x.gsub(/^set all_graphs/, 'root')}
 lines = lines.map {|x| x.gsub(/\./, 'dot')}
 lines = lines.map {|x| x.gsub(/\=/, 'equal')}
 lines = lines.map {|x| x.gsub(/\-\-/, 'minusminus')}
-lines = lines.map {|x| x.gsub(/ \-/, ' minus')}
+lines = lines.map {|x| x.gsub(/ \- /, ' minus ')}
 lines = lines.map {|x| x.gsub(/\+/, 'plus')}
-lines = lines.map {|x| x.gsub(/\-/, '_')}    # Underscores.
-lines = lines.map {|x| x.gsub(/^set all_graphs/, 'sql')}
+lines = lines.map {|x| x.gsub(/\-/, '_')}
 lines = lines.map {|x| x.gsub(/,/, 'comma')}
 lines = lines.map {|x| x.gsub(/\/\*/, 'comment_beg')}
 lines = lines.map {|x| x.gsub(/\*\//, 'comment_end')}
@@ -28,16 +27,14 @@ lines = lines.map {|x| x.gsub(/\//, '')}
 
 c = lines.join("")
 
-c = c.gsub(/\(\s*tailbranch/, "tailbranch(")
-c = c.gsub(/\(\s*stack/, "stack(")
-c = c.gsub(/\(\s*line /, "line( ")
-c = c.gsub(/\(\s*loop /, "loop( ")
-c = c.gsub(/\(\s*optx /, "optx( ")
-c = c.gsub(/\(\s*optx/, "optx(")
-c = c.gsub(/\(optx /, "optx( ")
-c = c.gsub(/\(\s*opt /, "opt( ")
-c = c.gsub(/\(\s*and /, "and( ")
-c = c.gsub(/\(\s*or /, "or( ")
+if true
+  c = c.gsub(/\(\s*tailbranch ?/, "tailbranch(")
+  c = c.gsub(/\(\s*opt(x?) ?/, "opt\\1( ")
+  c = c.gsub(/\(\s*or ?/, "or( ")
+  c = c.gsub(/\(\s*line ?/, "line( ")
+  c = c.gsub(/\(\s*loop ?/, "loop( ")
+  c = c.gsub(/\(\s*stack ?/, "stack(")
+end
 
 print c
 
