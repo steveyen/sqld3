2655535b37fbe742e161dc6fab65693a7d40813e
Steve Yen <steve.yen@gmail.com>
2011-04-16 10:28:57 -0700

    more in-progress sql-bubble.rb

--- sql-bubble.rb
+++ sql-bubble.rb
@@ -5,13 +5,14 @@
 bubble = ARGV[0] || 'sql-bubble.txt'
 
 lines = File.open(bubble, "r").readlines
-lines = lines.reject {|x| x.match /^\s*\#/} # Comments.
-lines = lines.reject {|x| x.match /^$\s+/}  # Whitespace.
+lines = lines.map {|x| x.gsub(/\{\}/, '')}
+lines = lines.reject {|x| x.match(/^\s*\#/)} # Comments.
+lines = lines.reject {|x| x.match(/^\s*$/)}  # Whitespace.
 lines = lines.map {|x| x.gsub(/\./, 'dot')}
 lines = lines.map {|x| x.gsub(/\=/, 'equal')}
-lines = lines.map {|x| x.gsub(/\+/, 'plus')}
-lines = lines.map {|x| x.gsub(/ \-/, ' minus')}
 lines = lines.map {|x| x.gsub(/\-\-/, 'minusminus')}
+lines = lines.map {|x| x.gsub(/ \-/, ' minus')}
+lines = lines.map {|x| x.gsub(/\+/, 'plus')}
 lines = lines.map {|x| x.gsub(/\-/, '_')}    # Underscores.
 lines = lines.map {|x| x.gsub(/^set all_graphs/, 'sql')}
 lines = lines.map {|x| x.gsub(/,/, 'comma')}
@@ -23,6 +24,20 @@ lines = lines.map {|x| x.gsub(/\(/, 'lparen')}
 lines = lines.map {|x| x.gsub(/\)/, 'rparen')}
 lines = lines.map {|x| x.gsub(/\{/, '(')}
 lines = lines.map {|x| x.gsub(/\}/, ')')}
+lines = lines.map {|x| x.gsub(/\//, '')}
 
-print lines
+c = lines.join("")
+
+c = c.gsub(/\(\s*tailbranch/, "tailbranch(")
+c = c.gsub(/\(\s*stack/, "stack(")
+c = c.gsub(/\(\s*line /, "line( ")
+c = c.gsub(/\(\s*loop /, "loop( ")
+c = c.gsub(/\(\s*optx /, "optx( ")
+c = c.gsub(/\(\s*optx/, "optx(")
+c = c.gsub(/\(optx /, "optx( ")
+c = c.gsub(/\(\s*opt /, "opt( ")
+c = c.gsub(/\(\s*and /, "and( ")
+c = c.gsub(/\(\s*or /, "or( ")
+
+print c
 
